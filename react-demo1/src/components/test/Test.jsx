2268c77fac1a990734c4d0d98d848a997bebc5f5
lk <919590347@qq.com>
2019-01-22 22:58:32 +0800

    学习了函数，点击传值

--- react-demo1/src/components/test/Test.jsx
+++ react-demo1/src/components/test/Test.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import Home from './../Home'
 import  './test.scss'
 // import pic from '../../assets/image/earth-demo.png'
-class Test extends React.Component {
+export default class Test extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
@@ -12,26 +14,42 @@ class Test extends React.Component {
                 { t:'t1' },
                 { t:'t2' },
                 { t:'t3' },
-            ]
+            ],
+            say: 'before change say',
+            style:{
+                backgroundColor: 'blue'
+            }
         }
-        this.style = {
-            backgroundColor: 'blue'
-        }
+    }
+    log(str) {
+        return console.log(str)
+    }
+    run = (getSay)=> {
+        console.log(getSay)
+        // this.state.say = getSay
+    }
+    changeSay = (sendToSay)=> {
+        console.log(sendToSay)
+        this.setState({
+            say: sendToSay
+        })
     }
     render() {
         var listResult = this.state.list1.map(function (v,k) {
             return <b key={k}>{v}</b>
         })
-        
         return (
+            <Router>
             <div className={`container`}>
                 <h2>componentName: {this.state.name}</h2>
                 请输入:<input type="text"/>
                 <ul className={`uls`}>
-                    <li style={{ backgroundColor: `yellow` }}>test1:</li>
-                    <li style={this.style}>test2</li>
+                    <li style={{ backgroundColor: `yellow` }}>
+                        <Link to={`/home`}>toHome</Link>
+                    </li>
+                    <li style={this.state.style}>test2</li>
                     <li>test3</li>
-                    {this.state.list2}
+                    {this.state.say}
                     <hr/>
                     {listResult}
                     <hr/>
@@ -40,9 +58,16 @@ class Test extends React.Component {
                             return <b key={k}>{v.t},</b>
                         })
                     }
+                    <hr/>
+                    <button onClick={this.run}>get say</button>
+                    <br/>
+                    <button onClick={this.changeSay.bind(this,'我是changeSay传过来的值')}>change say</button>
                 </ul>
+
+                <Route path={`/Home`} component={Home}></Route>
             </div>
+
+            </Router>
         )
     }
 }
-export  default Test
